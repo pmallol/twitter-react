d598efcd66365cf11c877198616e925cafcaa5c1
pmallol <paulamallol@gmail.com>
2024-05-05 02:39:53 +0200

    Add App component styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+import './App.css'
+
 export function App () {
   return (
-    <article>
-      <header>
-        <img src="https://unavatar.io/t0kt0" alt="profile-picture" />
-        <div>
+    <article className='tw-followCard'>
+      <header className='tw-followCard-header'>
+        <img className='tw-followCard-avatar' src="https://unavatar.io/t0kt0" alt="profile-picture" />
+        <div className='tw-followCard-info'>
           <strong>t0kt0</strong>
-          <span>@tokto</span>
+          <span className='tw-followCard-infoUserName'>@tokto</span>
         </div>
       </header>
 
       <aside>
-        <button>
+        <button className='tw-followCard-button'>
           Follow
         </button>
       </aside>
