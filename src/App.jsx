f5a03280cc7b6831dc0154db51079a863c376799
pmallol <paulamallol@gmail.com>
2024-05-05 02:29:20 +0200

    Add html markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 export function App () {
   return (
-    <div>Twitter React</div>
+    <article>
+      <header>
+        <img src="https://unavatar.io/t0kt0" alt="profile-picture" />
+        <div>
+          <strong>t0kt0</strong>
+          <span>@tokto</span>
+        </div>
+      </header>
+
+      <aside>
+        <button>
+          Follow
+        </button>
+      </aside>
+    </article>
   )
 
 }
