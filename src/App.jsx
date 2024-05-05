969c5f3e387c13283336ff338b0d4b79218a5607
pmallol <paulamallol@gmail.com>
2024-05-05 02:43:39 +0200

    Create TwitterFollowCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
 import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App () {
   return (
-    <article className='tw-followCard'>
-      <header className='tw-followCard-header'>
-        <img className='tw-followCard-avatar' src="https://unavatar.io/t0kt0" alt="profile-picture" />
-        <div className='tw-followCard-info'>
-          <strong>t0kt0</strong>
-          <span className='tw-followCard-infoUserName'>@tokto</span>
-        </div>
-      </header>
-
-      <aside>
-        <button className='tw-followCard-button'>
-          Follow
-        </button>
-      </aside>
-    </article>
+    <TwitterFollowCard/>
   )
 
 }
