083873d0c6e729255c1a9efcb697fa6ce1a1d03d
pmallol <paulamallol@gmail.com>
2024-05-05 17:17:47 -0300

    Render a list of users

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
+const users = [
+  { 
+    userName: 't0tkt0',
+    name: 'tokto.dev',
+    isFollowing: true
+  },
+  { 
+    userName: 'github',
+    name: 'Github',
+    isFollowing: false
+  },
+  { 
+    userName: 'code',
+    name: 'Visual Studio Code',
+    isFollowing: false
+  },
+  { 
+    userName: 'ThePracticalDev',
+    name: 'DEV Community',
+    isFollowing: false
+  }
+]
 export function App () {
   return (
     <section className='App'>
-      <TwitterFollowCard isFollowing userName='t0kt0' name='tokto.dev' />
-      <TwitterFollowCard isFollowing={false} userName='github' name='Github' />
-      <TwitterFollowCard isFollowing={false} userName='code' name='Visual Studio Code' />
-      <TwitterFollowCard isFollowing={false} userName='ThePracticalDev' name='DEV Community' />
+      {
+        users.map(user => {
+          const {userName, name, isFollowing} = user
+
+          return (
+            <TwitterFollowCard
+              key={userName}
+              isFollowing={isFollowing}
+              userName={userName} 
+              name={name}
+            />
+          )
+        })
+      }
     </section>
   )
 
